6be54f3b9243a74354f4b54109574414f8a07349
Mohammed Khaja Nawaz <123077970+Khaja-nawaz@users.noreply.github.com>
2025-06-09 11:07:52 +0530

    Update Variables.tf

--- Project-6/Source-code/Variables.tf
+++ Project-6/Source-code/Variables.tf
@@ -1,1 +1,23 @@
+variable "project_name" {
+  type    = string
+  default = "my-awesome-project"
+}
 
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_vpc" {
+  description = "aws_vpc_cidr_block"
+  type        = string
+  default     = "10.0.0.0/23"
+
+}
+
+variable "aws_vpc_cidr_block" {
+
+
+
+}
